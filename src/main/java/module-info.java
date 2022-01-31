f176d057fef22ba2caa2d4cb5c7a1955399c235f
Joshua "Shadow <ShadowLordAlpha@users.noreply.github.com>
2022-01-30 22:39:36 -0700

    gradle

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,6 @@ module starcraft.component.manager {
     requires com.github.ocraft.s2client.bot;
     requires com.github.ocraft.s2client.protocol;
     requires lombok;
-    
+
     exports com.shadowlordalpha.sc2.manager;
 }
